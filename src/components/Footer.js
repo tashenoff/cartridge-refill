ed449df2a9e6588d47d2a2a6fc8f1680d8bd07c4
tashenoff <kail.dead.kd@gmail.com>
2024-10-29 16:12:35 +0600

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <footer className="bg-base-200 text-base-content p-4">
       <div className="container mx-auto text-center">
-      <ScrollArrow className='bottom-10 bg-primary relative w-10 rounded-lg' direction="top" targetId="services" />
+      <ScrollArrow className='bottom-10 bg-opacity-20 backdrop-blur-md  bg-primary relative w-10 rounded-lg' direction="top" targetId="services" />
         <p>&copy; 2024 Заправка картриджей. Все права защищены.</p>
         <p>Телефон: 8(700)961-96-63</p>
       </div>
